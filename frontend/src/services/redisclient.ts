7252702190889efa00b0a118c751cdae82dcb320
Victor Lingenthal <victor.lingenthal@gmail.com>
2021-05-21 19:22:12 +0100

    Server Cleanup

--- frontend/src/services/redisclient.ts
+++ frontend/src/services/redisclient.ts
@@ -5,7 +5,7 @@ const Redis = require('ioredis')
 let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 
 const options = {
-  retryStrategy: times => Math.min(times * 50, 2000)
+  retryStrategy: (times :number) => Math.min(times * 50, 2000)
 };
 
 export const pubSub = new RedisPubSub({
